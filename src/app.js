1a10a9ae74d11df5491114234c3701206743842d
jairisonsouza <jairison.analistadesistemas@gmail.com>
2023-09-10 15:41:50 -0300

    Criando rota para atualizar livros

--- src/app.js
+++ src/app.js
@@ -1,17 +1,24 @@
 import express, { json } from 'express';
 
 const app = express();
+app.use(express.json());
 
 const livros = [
     {
-        "id" : "1",
+        "id" : 1,
         "nome" : "O Senhor dos Anéis"
     },
     {
-        "id" : "2",
+        "id" : 2,
         "nome" : "O Hobbit"
     }
 ];
+
+function buscaLivro(id) {
+    return livros.findIndex(livro => {
+        return livro.id === Number(id)
+    })
+}
 
 app.get('/', (req, res) => {
     res.status(200).send("Curso de Node.js");
@@ -20,5 +27,21 @@ app.get('/', (req, res) => {
 app.get('/livros', (req, res) => {
     res.status(200).json(livros)
 });
+
+app.get('/livros/:id', (req, res) => {
+    const index = buscaLivro(req.params.id);
+    res.status(200).json(livros[index]);
+});
+
+app.post('/livros', (req, res) => {
+    livros.push(req.body);
+    res.status(201).send("Livro cadastrado com sucesso!");
+});
+
+app.put('/livros/:id', (req, res) => {
+    const index = buscaLivro(req.params.id);
+    livros[index].nome = req.body.nome;
+    res.status(200).json(livros);
+})
 
 export default app;
