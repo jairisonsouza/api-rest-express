86434585cda4f0e1ee0a9066ac77aa53f1abcf27
jairisonsouza <jairison.analistadesistemas@gmail.com>
2023-09-11 10:11:04 -0300

    Criando Controllers e Routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-import express, { json } from 'express';
+import express from 'express';
 import conectaNaDatabase from './config/dbConnect.js';
-import livro from './models/Livro.js';
+import routes from './routes/index.js';
 
 const conexao = await conectaNaDatabase();
 
@@ -9,52 +9,10 @@ conexao.on("error", (erro) => {
 });
 
 conexao.once("open", () => {
-    console.log("Conexão com o banco feita com sucesso!")
-})
+    console.log("Conexão com o banco feita com sucesso!");
+});
 
 const app = express();
-app.use(express.json());
-
-const livros = [
-    {
-        "id" : 1,
-        "titulo" : "O Senhor dos Anéis"
-    },
-    {
-        "id" : 2,
-        "titulo" : "O Hobbit"
-    }
-];
-
-app.get('/', (req, res) => {
-    res.status(200).send("Curso de Node.js");
-});
-
-app.get('/livros', async (req, res) => {
-    const listaLivros = await livro.find({});
-    res.status(200).json(listaLivros);
-});
-
-app.get('/livros/:id', (req, res) => {
-    const index = buscaLivro(req.params.id);
-    res.status(200).json(livros[index]);
-});
-
-app.post('/livros', (req, res) => {
-    livros.push(req.body);
-    res.status(201).send("Livro cadastrado com sucesso!");
-});
-
-app.put('/livros/:id', (req, res) => {
-    const index = buscaLivro(req.params.id);
-    livros[index].titulo = req.body.titulo;
-    res.status(200).json(livros);
-});
-
-app.delete('/livros/:id', (req, res) => {
-    const index = buscaLivro(req.params.id);
-    livros.splice(index, 1);
-    res.status(200).send("Livro removido com sucesso!");
-});
+routes(app);
 
 export default app;
